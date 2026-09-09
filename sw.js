aab8c9212d0d7e11e1b6909ad7681a3e71066f60
pfmpjdp <pfmp.jdp@gmail.com>
2026-09-09 16:15:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="pfmp-jdp-v6-6-2-design";
+const CACHE="pfmp-jdp-v6-7-corrections";
 self.addEventListener("install",e=>{self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{
